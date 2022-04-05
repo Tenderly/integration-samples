79cbed3cf1b624e9573e6fd60fb818e8df291104
Filip Petrovic <filip.petroviccc@gmail.com>
2022-04-05 11:52:40 +0200

    ci cd for smart contracts

---
+++ ci-cd-pipeline-for-smart-contracts/smart-contract-pipeline/hardhat.config.ts
@@ -0,0 +1,24 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@tenderly/hardhat-tenderly"
+
+const {
+    TENDERLY_FORK_ID
+} = process.env
+
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+export default {
+  solidity: "0.8.4",
+  networks: {
+    "tenderly-fork": {
+      url: `https://rpc.tenderly.co/fork/${TENDERLY_FORK_ID}`
+    },
+  },
+};
